b0ed1c8b72b0d7138c93c5030151dd3f117f8f47
Thorsten Hoeger <thorsten.hoeger@taimos.de>
2023-01-28 01:06:11 +0100

    chore: add serverless lib

--- .projenrc.js
+++
@@ -1,9 +0,0 @@
-const { awscdk } = require('projen');
-
-const project = new awscdk.AwsCdkTypeScriptApp({
-  cdkVersion: '2.60.0',
-  defaultReleaseBranch: 'main',
-  name: 'event-system-backend',
-});
-
-project.synth();
---
+++ .projenrc.ts
@@ -0,0 +1,14 @@
+import { ServerlessProject } from '@taimos/cdk-serverless-v2/lib/projen';
+
+const project = new ServerlessProject({
+  cdkVersion: '2.60.0',
+  defaultReleaseBranch: 'main',
+  name: 'event-system-backend',
+  deps: [
+    'projen',
+    '@taimos/cdk-serverless-v2',
+    'date-fns',
+  ],
+});
+
+project.synth();
