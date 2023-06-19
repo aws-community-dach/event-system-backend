4bd799647cc165e78865b27d2b32f6a434371078
timros1412 <91313114+timros1412@users.noreply.github.com>
2023-06-19 08:49:23 +0200

    fix: search through primary index, return statement

--- src/lambda/rest.eventmgmtapi.deleteEvent.ts
+++ src/lambda/rest.eventmgmtapi.deleteEvent.ts
@@ -1,6 +1,6 @@
 import { api, errors } from 'cdk-serverless/lib/lambda';
 import { operations } from '../generated/rest.eventmgmtapi-model.generated';
-import { Event, Index_GSI1_Name } from '../generated/datastore.event-model.generated';
+import { Event, Index_primary_Name } from '../generated/datastore.event-model.generated';
 
 export const handler = api.createOpenApiHandler<operations['deleteEvent']>(async (ctx) => {
   ctx.logger.info(JSON.stringify(ctx.event));
@@ -8,13 +8,12 @@ export const handler = api.createOpenApiHandler<operations['deleteEvent']>(async
   ctx.auth.assertAdmin();
 
   const id = ctx.event.pathParameters!.eventID;
-  const event = await Event.get({ id }, { index: Index_GSI1_Name });
+  const event = await Event.get({ id }, { index: Index_primary_Name });
+  console.log(event)
   if (!event) {
     throw new errors.NotFoundError();
   }else{
-    await Event.remove({ id }, { index: Index_GSI1_Name });
-    return {
-      statusCode: 204
-    }
+    await Event.remove({ id }, { index: Index_primary_Name });
+    return;
   }
 });
