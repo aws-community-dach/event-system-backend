00b1c636ecd1d8294762e77f28e9cdefdc2b27d0
timros1412 <91313114+timros1412@users.noreply.github.com>
2023-06-19 11:32:47 +0200

    feat: listEventParticipantImplementation

--- src/lambda/rest.eventmgmtapi.listEventParticipants.ts
+++ src/lambda/rest.eventmgmtapi.listEventParticipants.ts
@@ -1,8 +1,27 @@
 import { api, errors } from 'cdk-serverless/lib/lambda';
+import { Participant, Event, Index_GSI1_Name} from '../generated/datastore.event-model.generated';
 import { operations } from '../generated/rest.eventmgmtapi-model.generated';
 
 export const handler = api.createOpenApiHandler<operations['listEventParticipants']>(async (ctx) => {
   ctx.logger.info(JSON.stringify(ctx.event));
 
-  throw new errors.HttpError(500, 'Not yet implemented');
+  //ctx.auth.assertAdmin();
+
+  const id = ctx.event.pathParameters!.eventID;
+  const event = await Event.get({ id: id }, { index: Index_GSI1_Name });
+  if (!event) {
+    throw new errors.NotFoundError();
+  }
+
+  const participants = await Participant.find({eventId: id});
+
+  return participants.map(participant => ({
+    id: participant.eventId,
+    name: participant.name,
+    displayName: participant.displayName,
+    email: participant.email,
+    customData: participant.customData,
+    confirmationId: participant.token
+
+  }))
 });
