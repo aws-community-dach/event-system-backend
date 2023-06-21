f8b75e903d99699f3d86befcec92cf228165d688
timros1412 <91313114+timros1412@users.noreply.github.com>
2023-06-21 14:46:57 +0200

    fix: removed deleteEvent implementation from this branch

--- src/lambda/rest.eventmgmtapi.deleteEvent.ts
+++ src/lambda/rest.eventmgmtapi.deleteEvent.ts
@@ -1,19 +1,8 @@
 import { api, errors } from 'cdk-serverless/lib/lambda';
 import { operations } from '../generated/rest.eventmgmtapi-model.generated';
-import { Event, Index_primary_Name } from '../generated/datastore.event-model.generated';
 
 export const handler = api.createOpenApiHandler<operations['deleteEvent']>(async (ctx) => {
   ctx.logger.info(JSON.stringify(ctx.event));
 
-  ctx.auth.assertAdmin();
-
-  const id = ctx.event.pathParameters!.eventID;
-  const event = await Event.get({ id }, { index: Index_primary_Name });
-  console.log(event)
-  if (!event) {
-    throw new errors.NotFoundError();
-  }else{
-    await Event.remove({ id }, { index: Index_primary_Name });
-    return;
-  }
+  throw new errors.HttpError(500, 'Not yet implemented');
 });
