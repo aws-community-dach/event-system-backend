fcb2554cce08017ed277b8c75b3c13ec593620b1
timros1412 <91313114+timros1412@users.noreply.github.com>
2023-06-22 09:37:27 +0200

    feat: deleteEvent funciton

--- src/lambda/rest.eventmgmtapi.deleteEvent.ts
+++ src/lambda/rest.eventmgmtapi.deleteEvent.ts
@@ -1,8 +1,23 @@
 import { api, errors } from 'cdk-serverless/lib/lambda';
+import { Event, Participant } from '../generated/datastore.event-model.generated';
 import { operations } from '../generated/rest.eventmgmtapi-model.generated';
+
 
 export const handler = api.createOpenApiHandler<operations['deleteEvent']>(async (ctx) => {
   ctx.logger.info(JSON.stringify(ctx.event));
 
-  throw new errors.HttpError(500, 'Not yet implemented');
+  ctx.auth.assertAdmin();
+
+  const id = ctx.event.pathParameters!.eventID;
+  const participants = await Participant.find({eventId: id})
+  if(participants.length > 0){
+    throw new errors.ForbiddenError();
+  }
+  const event = await Event.get({ id });
+  if (!event) {
+    throw new errors.NotFoundError();
+  }
+  await Event.remove({ id });
+  return;
+
 });
