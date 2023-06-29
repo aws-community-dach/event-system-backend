3f34b14fa22d5997fdc2eae47b32b4172db17f4f
timros1412 <91313114+timros1412@users.noreply.github.com>
2023-06-29 15:08:11 +0200

    feat: theoretical updateEventParticipant implementation

--- src/lambda/rest.eventmgmtapi.updateEventParticipant.ts
+++ src/lambda/rest.eventmgmtapi.updateEventParticipant.ts
@@ -1,8 +1,28 @@
 import { api, errors } from 'cdk-serverless/lib/lambda';
+import { Participant } from '../generated/datastore.event-model.generated';
 import { operations } from '../generated/rest.eventmgmtapi-model.generated';
 
 export const handler = api.createOpenApiHandlerWithRequestBody<operations['updateEventParticipant']>(async (ctx, data) => {
   ctx.logger.info(JSON.stringify(ctx.event));
   ctx.logger.info(JSON.stringify(data));
-  throw new errors.HttpError(500, 'Not yet implemented');
+
+  const participantId = ctx.event.pathParameters!.participantID;
+  const eventId = ctx.event.pathParameters!.eventID;
+  const token = ctx.event.queryStringParameters!.token;
+
+  const participant = await Participant.get({ eventId: eventId, email: participantId });
+  if (!participant || (token !== participant.token && !ctx.auth.isAdmin())) {
+    throw new errors.NotFoundError();
+  }
+  
+  await Participant.update({
+    eventId: eventId,
+    email: participantId,
+    name: data.name,
+    displayName: data.displayName,
+    customData: JSON.stringify(data.customData)
+  })
+
+
+  ctx.response.statusCode = 200;
 });
