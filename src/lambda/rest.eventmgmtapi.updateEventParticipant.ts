68a8d94213101fe3752290e269469841d8a669d3
Eduard Schwarzkopf <eschwarzkopf@evoila.de>
2024-04-26 12:10:38 +0200

    pathParameters is case sensitive, needs to be ID

--- src/lambda/rest.eventmgmtapi.updateEventParticipant.ts
+++ src/lambda/rest.eventmgmtapi.updateEventParticipant.ts
@@ -2,15 +2,23 @@ import { api, errors } from 'cdk-serverless/lib/lambda';
 import { Participant } from '../generated/datastore.event-model.generated';
 import { operations } from '../generated/rest.eventmgmtapi-model.generated';
 
-export const handler = api.createOpenApiHandlerWithRequestBody<operations['updateEventParticipant']>(async (ctx, data) => {
+export const handler = api.createOpenApiHandlerWithRequestBody<
+  operations['updateEventParticipant']
+>(async (ctx, data) => {
   ctx.logger.info(JSON.stringify(ctx.event));
   ctx.logger.info(JSON.stringify(data));
 
   const eventId = ctx.event.pathParameters!.eventID;
-  const participantId = ctx.event.pathParameters!.participantId;
+  const participantId = ctx.event.pathParameters!.participantID;
 
-  const participant = await Participant.get({ eventId: eventId, participantId: participantId });
-  if (!participant || (participantId !== participant.participantId && !ctx.auth.isAdmin())) {
+  const participant = await Participant.get({
+    eventId: eventId,
+    participantId: participantId,
+  });
+  if (
+    !participant ||
+    (participantId !== participant.participantId && !ctx.auth.isAdmin())
+  ) {
     throw new errors.NotFoundError();
   }
 
