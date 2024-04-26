c45834456bc854b14ca76e07836a735cf5c208d7
Eduard Schwarzkopf <eschwarzkopf@evoila.de>
2024-04-26 12:16:35 +0200

    fix formatting

--- src/lambda/rest.eventmgmtapi.getEventParticipant.ts
+++ src/lambda/rest.eventmgmtapi.getEventParticipant.ts
@@ -2,22 +2,14 @@ import { api, errors } from 'cdk-serverless/lib/lambda';
 import { Participant } from '../generated/datastore.event-model.generated';
 import { operations } from '../generated/rest.eventmgmtapi-model.generated';
 
-export const handler = api.createOpenApiHandler<
-  operations['getEventParticipant']
->(async (ctx) => {
+export const handler = api.createOpenApiHandler<operations['getEventParticipant']>(async (ctx) => {
   ctx.logger.info(JSON.stringify(ctx.event));
 
   const eventId = ctx.event.pathParameters!.eventID;
   const participantId = ctx.event.pathParameters!.participantID;
 
-  const participant = await Participant.get({
-    eventId: eventId,
-    participantId: participantId,
-  });
-  if (
-    !participant ||
-    (participantId !== participant.participantId && !ctx.auth.isAdmin())
-  ) {
+  const participant = await Participant.get({ eventId: eventId, participantId: participantId });
+  if (!participant || (participantId !== participant.participantId && !ctx.auth.isAdmin())) {
     throw new errors.NotFoundError();
   }
 
